424ff0acd096877d986989894c1b85fbb7cfbe3e
fatizaheracompus <fatimaaitbrahim18@gmail.com>
2023-09-25 14:02:41 -0700

    task0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+
+/**
+ *bubble_sort - Function that sorted an array of integers
+ *@array: array of integers
+ *@size: the size of our array
+ *return: void.
+ */
+void bubble_sort(int **array, size_t size)
+{
+int tmp;
+size_t x = 0, y = 0;
+
+if (array == NULL || size == NULL)
+return;
+	while (x < size)
+	{
+		for (y = 0; y < size - 1; y++)
+		{
+			if (array[y] > array[y + 1])
+			{
+				tmp = array[y];
+				array[y] = array[y + 1];
+				array[y + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
+}
+
+
+
