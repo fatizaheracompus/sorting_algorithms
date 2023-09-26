8eb548eecdad3c9e072a1c22e9a69a4271ad0054
fatizaheracompus <fatimaaitbrahim18@gmail.com>
2023-09-26 13:41:36 -0700

    task3

---
+++ 2-selection_sort.c
@@ -0,0 +1,37 @@
+#include "sort.h"
+
+/**
+ *selection_sort - function that sort in array of integer
+ *@array: this an array of integers
+ *@size: this is a size of integers
+ *
+ *Return: void
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t a, b, c;
+	int tmp;
+
+	if (array == NULL || !size)
+		return;
+	for (a = 0; a < size - 1; a++)
+	{
+		for (b = size - 1, c = a + 1; b > a; b--)
+		{
+			if (array[b] < array[c])
+			{
+				c = b;
+			}
+		}
+		if (array[a] > array[c])
+		{
+			tmp = array[a];
+			array[a] = array[b];
+			array[c] = tmp;
+			print_array(array, size);
+		}
+	}
+}
+
+
+
